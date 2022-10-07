007fd34e6e1ea4878780539b6cc41cd43a2404fb
ABCoding1704 <81834645+undefined@users.noreply.github.com>
2022-10-07 14:15:11 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# TooltipButtonControl-CustomComponent
-Created with Components.studio
+# Vue 3.0.0-beta Starter Kit
+
+### Links
+
+- [GitHub (https://github.com/vuejs/vue-next)](https://github.com/vuejs/vue-next)
---
+++ src/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { reactive } from 'vue';
+
+	export default defineComponent({
+		data() {
+
+			return {
+
+			}
+		},
+		setup() {
+			
+
+			return {
+				
+			}
+		}
+	});
+</script>
+
+<template>
+	<div>
+
+	</div>
+</template>
+
+
+<style scoped lang="scss">
+
+</style>
